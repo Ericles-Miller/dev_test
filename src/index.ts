209b8bc1f76d07ec9e71f3b06babdd0d6d5950b7
Ericles-Millere <ericlesmiller15@gmail.com>
2024-11-15 00:24:56 -0300

    fix: alter port api

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ initializeDatabase();
 
 app.use(router);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
